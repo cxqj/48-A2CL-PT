bcc2804feeea9c21d2125f13e33f0423b90f9522
cxqj <43702702+cxqj@users.noreply.github.com>
2020-09-23 20:35:28 +0800

    Update model.py

--- model.py
+++ model.py
@@ -40,37 +40,38 @@ class Model(nn.Module):
         self.mul_f = nn.Parameter(data=torch.ones(num_class))
 
     def forward(self, inputs):
-        N, T, D = inputs.shape
+        N, T, D = inputs.shape  # 32,200,2048
         D //= 2
         x_r = F.relu(self.fc_r(inputs[:,:,:D]))
         x_f = F.relu(self.fc_f(inputs[:,:,D:]))
-        x_r = F.relu(self.fc1_r(x_r)).permute(0,2,1)
-        x_f = F.relu(self.fc1_f(x_f)).permute(0,2,1)
+        x_r = F.relu(self.fc1_r(x_r)).permute(0,2,1)  # (32,1024,200)
+        x_f = F.relu(self.fc1_f(x_f)).permute(0,2,1)  # (32,1024,200)
 
         x_r = self.dropout_r(x_r)
         x_f = self.dropout_f(x_f)
 
-        k = max(T-floor(T/self.s), 1)
-        cls_x_r = self.classifier_r(x_r).permute(0,2,1)
-        cls_x_f = self.classifier_f(x_f).permute(0,2,1)
-        cls_x_ra = cls_x_r.new_zeros(cls_x_r.shape)
-        cls_x_fa = cls_x_f.new_zeros(cls_x_f.shape)
-        cls_x_rat = cls_x_r.new_zeros(cls_x_r.shape)
-        cls_x_fat = cls_x_f.new_zeros(cls_x_f.shape)
+        k = max(T-floor(T/self.s), 1)  # 195
+        cls_x_r = self.classifier_r(x_r).permute(0,2,1)   # (32,200,101)
+        cls_x_f = self.classifier_f(x_f).permute(0,2,1)   # (32,200,101)
+        cls_x_ra = cls_x_r.new_zeros(cls_x_r.shape)  # (32,200,101)
+        cls_x_fa = cls_x_f.new_zeros(cls_x_f.shape)  # (32,200,101)
+        cls_x_rat = cls_x_r.new_zeros(cls_x_r.shape) # (32,200,101)
+        cls_x_fat = cls_x_f.new_zeros(cls_x_f.shape) # (32,200,101)
 
         mask_value = -100
 
         for i in range(self.num_class):
-            mask_r = cls_x_r[:,:,i]>torch.kthvalue(cls_x_r[:,:,i], k, dim=1, keepdim=True)[0]
-            x_r_erased = torch.masked_fill(x_r, mask_r.unsqueeze(1), 0)
-            cls_x_ra[:,:,i] = torch.masked_fill(self.classifier_ra[i](x_r_erased).squeeze(1), mask_r, mask_value)
-            cls_x_rat[:,:,i] = self.classifier_ra[i](x_r).squeeze(1)
+            mask_r = cls_x_r[:,:,i]>torch.kthvalue(cls_x_r[:,:,i], k, dim=1, keepdim=True)[0]   # (32,200)
+            x_r_erased = torch.masked_fill(x_r, mask_r.unsqueeze(1), 0)  # (32,1024,200)
+            cls_x_ra[:,:,i] = torch.masked_fill(self.classifier_ra[i](x_r_erased).squeeze(1), mask_r, mask_value)  # (32,200,101)
+            cls_x_rat[:,:,i] = self.classifier_ra[i](x_r).squeeze(1)  # (32,200,101)
 
-            mask_f = cls_x_f[:,:,i]>torch.kthvalue(cls_x_f[:,:,i], k, dim=1, keepdim=True)[0]
-            x_f_erased = torch.masked_fill(x_f, mask_f.unsqueeze(1), 0)
-            cls_x_fa[:,:,i] = torch.masked_fill(self.classifier_fa[i](x_f_erased).squeeze(1), mask_f, mask_value)
-            cls_x_fat[:,:,i] = self.classifier_fa[i](x_f).squeeze(1)
+            mask_f = cls_x_f[:,:,i]>torch.kthvalue(cls_x_f[:,:,i], k, dim=1, keepdim=True)[0]  # (32,200)
+            x_f_erased = torch.masked_fill(x_f, mask_f.unsqueeze(1), 0)   # (32,1024,200)
+            cls_x_fa[:,:,i] = torch.masked_fill(self.classifier_fa[i](x_f_erased).squeeze(1), mask_f, mask_value)  # (32,200,101)
+            cls_x_fat[:,:,i] = self.classifier_fa[i](x_f).squeeze(1)   # (32,200,101)
 
-        tcam = (cls_x_r+cls_x_rat*self.omega) * self.mul_r + (cls_x_f+cls_x_fat*self.omega) * self.mul_f
+        tcam = (cls_x_r+cls_x_rat*self.omega) * self.mul_r + (cls_x_f+cls_x_fat*self.omega) * self.mul_f  # # (32,200,101)
 
+        # (32,200,1024) [(32,200,101),(32,200,101)], (32,200,1024) [(32,200,101),(32,200,101)],  (32,200,101)
         return x_r.permute(0,2,1), [cls_x_r, cls_x_ra], x_f.permute(0,2,1), [cls_x_f, cls_x_fa], tcam
